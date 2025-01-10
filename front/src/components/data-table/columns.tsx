121271b8429117352cbe30803229545b1c776e76
quentin <quentinderycke6@gmail.com>
2025-01-10 11:02:02 +0100

    feat(front): :construction: sorting in data table

--- front/src/components/data-table/columns.tsx
+++ front/src/components/data-table/columns.tsx
@@ -1,13 +1,25 @@
 import {  MergedCryptoData } from "@/data/interface/coin"
 import { ColumnDef } from "@tanstack/react-table"
-
-
+import { ArrowUpDown } from "lucide-react"
+import { Button } from "../ui/button"
+  
 
 export const columns: ColumnDef<MergedCryptoData>[] = [
 
   {
+    
     accessorKey: "name",
-    header: "Name",
+    header: ({ column }) => {
+      return (
+        <Button
+        variant="outline"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
   },
   {
     accessorKey: "symbol",
--- front/src/components/data-table/data-table.tsx
+++ front/src/components/data-table/data-table.tsx
@@ -1,5 +1,6 @@
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { ColumnDef, flexRender, getCoreRowModel, SortingState, useReactTable } from "@tanstack/react-table";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../ui/table";
+import { useState } from "react";
 
 interface DataTableProps<TData, TValue> {
 
@@ -12,10 +13,15 @@ export function DataTable<TData, TValue>({
     columns,
     data
 }: DataTableProps<TData, TValue>) {
+    const [sorting, setSorting] = useState<SortingState>([])
     const table = useReactTable({
         data,
         columns,
-        getCoreRowModel: getCoreRowModel()
+        onSortingChange: setSorting,
+        getCoreRowModel: getCoreRowModel(),
+        state: {
+            sorting,
+          },
     });
 
     return (
